387cebfe95ebfa1cd99188611397295551514b48
Tuomo Pelkonen <tuomo.pelkonen@qt.io>
2021-10-07 09:24:50 +0300

    Checkout 6.0 branch for QDS 2.3
    Change-Id: Iaa5de2cf18113cd4afaedf944ae77aff6d384a96
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- init-repository.sh
+++ init-repository.sh
@@ -2,14 +2,14 @@
 git submodule update --init --recursive
 
 cd qtcreator
-git checkout --track origin/5.0
+git checkout --track origin/6.0
 gitdir=$(git rev-parse --git-dir); scp -P 29418 codereview.qt-project.org:hooks/commit-msg ${gitdir}/hooks/
 cd ..
 cd qtquickdesigner
-git checkout --track origin/5.0
+git checkout --track origin/6.0
 gitdir=$(git rev-parse --git-dir); scp -P 29418 codereview.qt-project.org:hooks/commit-msg ${gitdir}/hooks/
 cd ..
 cd telemetry
-git checkout --track origin/5.0
+git checkout --track origin/6.0
 gitdir=$(git rev-parse --git-dir); scp -P 29418 codereview.qt-project.org:hooks/commit-msg ${gitdir}/hooks/
 cd ..
